265d9c50b58935baabf1950c649c5e06fc47faa0
root <dherault@gmail.com>
2015-10-05 14:45:48 +0000

    back to gm

---
+++ _misc/c9_init_david/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A script to initialize a cloud9 env for a given developer
+
+git config --global user.email "dherault@gmail.com"
+cp /root/aquest/_misc/c9_init_david/project.settings /root/aquest/.c9/
